6bb1c9be869c7235371ffee60681de3df153e754
JiriKolarik <jiri.kolarik@imin.cz>
2014-02-20 22:57:40 +0100

    title is not required

--- app/models/scenery/flexslider/translation.rb
+++ app/models/scenery/flexslider/translation.rb
@@ -1,7 +1,7 @@
 module Scenery
   class Flexslider::Translation < Globalize::ActiveRecord::Translation 
     extend Enumerize
-    validates :title, length: { maximum: 255 }, presence: true
+    validates :title, length: { maximum: 255 }
     validates :locale, presence: true, uniqueness: { scope: :scenery_flexslider_id }
 
     enumerize :locale, in: I18n.available_locales
